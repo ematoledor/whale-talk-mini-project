337be0ee3c6a7a512047837ed4f0ee5ac48b8bcd
Emanuel Toledo <ematolr@gmail.com>
2021-05-31 19:21:27 -0500

    working on whale trnaslator codecademy mini project.

---
+++ main.js
@@ -0,0 +1,7 @@
+// phrase to translate to whale language.
+
+let input = 'Hello, I am Emanuel. Curreently living at La Casa Casa Merida.';
+
+// Array of vowels.
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
