5885b5d33aea20f8e957b0e146782e9e0bfa22b6
Emanuel Toledo <ematolr@gmail.com>
2021-05-31 22:25:35 -0500

    Working on loops to evaluate similar vowels.

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ let input = 'Hello, I am Emanuel. Curreently living at La Casa Casa Merida.';
 // Array of vowels.
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// Array that will contain related input.
+
+let resultArray = [];
+
+// loop to go through the arrays and evaluate. Also push related to new array.
+
+for (i = 0; i < input.length; i++) {
+  // console.log(input[i]);
+  // testing output... working fine.
+  for (j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      if (input[i] === 'e') {
+        resultArray.push('ee')
+      }
+    }
+  }
+}
