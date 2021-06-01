4139a6210ec9356c42c176766313bc5bcbf541a1
Emanuel Toledo <ematolr@gmail.com>
2021-05-31 23:13:15 -0500

    refactored code to use for...of

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 // phrase to translate to whale language.
-let input = 'Hello, I am Emanuel. Currently living at La Casa Casa Merida.';
+let input = 'Hi, human.';
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 let resultArray = [];
 
-// loop to go over first array.
-for (i = 0; i < input.length; i++) {
-  // Loop to go through second array.
-  for (j = 0; j < vowels.length; j++) {
-    // Analyze is values are the same
-    if (input[i] === vowels[j]) {
-      // Double the letter if its 'e' or 'u' and push any of the cases to new array.
-      if (input[i] === 'e') {
+// // loop to go over first array.
+// for (i = 0; i < input.length; i++) {
+//   // Loop to go through second array.
+//   for (j = 0; j < vowels.length; j++) {
+//     // Analyze is values are the same
+//     if (input[i] === vowels[j]) {
+//       // Double the letter if its 'e' or 'u' and push any of the cases to new array.
+//       if (input[i] === 'e') {
+//         resultArray.push('EE');
+//       }
+//       else if (input[i] === 'u') {
+//         resultArray.push('UU');
+//       } else {
+//         resultArray.push(input[i]);
+//       }
+//     }
+//   }
+// }
+
+// REFACTORING PREVIOUS CODE TO USE FOR...OF >
+
+for (const item of input) {
+  for (const jtem of vowels) {
+    if (item === jtem) {
+      if (item === 'e') {
         resultArray.push('EE');
       }
-      else if (input[i] === 'u') {
+      else if (item === 'u') {
         resultArray.push('UU');
       } else {
-        resultArray.push(input[i]);
+        resultArray.push(item);
       }
     }
   }
 }
+
 
 // brings string togetther and turns to upper case.
 console.log(resultArray.join('').toUpperCase());
